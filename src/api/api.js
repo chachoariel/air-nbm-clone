37760f5289bcf40b2f3aea065e57b97c75a11898
Alfredo Ariel Diaz <alfredodiazjjy@gmail.com>
2023-04-14 16:39:48 -0300

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+const API_VERSION_NUMBER = 1
+
+export const api = {
+  server: 'https://6430c2d83adb1596515fcff4.mockapi.io/', // completa con api server
+  apiVersion: `/api/v${API_VERSION_NUMBER}`
+}
+// https://6435ae5f537112453fdcf779.mockapi.io
